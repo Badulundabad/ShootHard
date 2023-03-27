04aacceb4cd9cabbf07022d014c3ab6f192c4038
DMITRIY GRIN <greendmitriy407@gmail.com>
2023-03-27 16:59:39 +0300

    added ShotJob

---
+++ Assets/_App/Scripts/ShotJob.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+[BurstCompile]
+partial struct ShotJob : IJobEntity
+{
+    [ReadOnly] public ComponentLookup<LocalToWorld> LocalToWorldLookup;
+    public quaternion Direction;
+    public EntityCommandBuffer ECB;
+
+    [BurstCompile]
+    void Execute(in WeaponAspect weaponAspect)
+    {
+        Entity instance = ECB.Instantiate(weaponAspect.BulletPrefab);
+
+        LocalToWorld localToWorld = LocalToWorldLookup[weaponAspect.SpawnPoint];
+        LocalTransform localTransform = LocalTransform.FromPositionRotationScale(localToWorld.Position, Direction, 0.2f);
+
+        ECB.AddComponent(instance, new SpeedComponent()
+        { 
+            Direction = localTransform.Forward(),
+            Value = weaponAspect.Weapon.ValueRO.BulletSpeed
+        });
+        ECB.SetComponent(instance, localTransform);
+    }
+}
