7d2dc7d57718446740e087318c82db7ebd9e9c08
DMITRIY GRIN <greendmitriy407@gmail.com>
2023-03-27 17:01:02 +0300

    added attack logic to PlayerInputSystem

--- Assets/_App/Scripts/PlayerInputSystem.cs
+++ Assets/_App/Scripts/PlayerInputSystem.cs
@@ -23,15 +23,39 @@ public partial class PlayerInputSystem : SystemBase
         Vector2 value = _input.GamePlay.Move.ReadValue<Vector2>() * SystemAPI.Time.DeltaTime;
         float3 moveDelta = new float3(value.x, 0, value.y);
 
-        foreach (var (speed, localTransform, localToWorld) in SystemAPI.Query<SpeedComponent, RefRW<LocalTransform>, LocalToWorld>().WithAll<PlayerComponent>())
+        foreach (var (weapon, speed, localTransform, localToWorld) in SystemAPI.Query<RefRW<Weapon>, SpeedComponent, RefRW<LocalTransform>, LocalToWorld>().WithAll<PlayerComponent>())
         {
             localTransform.ValueRW.Position += moveDelta * speed.Value;
 
             Ray ray = _camera.ScreenPointToRay(Mouse.current.position.value);
             if (Physics.Raycast(ray, out RaycastHit hit, 100))
             {
-                localTransform.ValueRW.Rotation = Helpers.LookAtRotation(localToWorld.Position, hit.point, new float3(0, 1, 0));
+                float3 hitPoint = hit.point;
+                hitPoint.y = localToWorld.Position.y;
+                localTransform.ValueRW.Rotation = Helpers.LookAtRotation(localToWorld.Position, hitPoint, new float3(0, 1, 0));
+            }
+
+            if (_input.GamePlay.Attack.IsPressed())
+            {
+                weapon.ValueRW.Cooldown -= SystemAPI.Time.DeltaTime;
+                if (weapon.ValueRO.Cooldown <= 0)
+                {
+                    weapon.ValueRW.Cooldown = weapon.ValueRO.FiringRate;
+                    Attack(localTransform.ValueRO.Rotation);
+                }
             }
         }
+    }
+
+    private void Attack(quaternion direction)
+    {
+        var ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+
+        new ShotJob
+        {
+            Direction = direction,
+            LocalToWorldLookup = GetComponentLookup<LocalToWorld>(true),
+            ECB = ecb.CreateCommandBuffer(EntityManager.WorldUnmanaged)
+        }.Schedule();
     }
 }
